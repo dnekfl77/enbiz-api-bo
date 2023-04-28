8ab0d557fbe29eab3e3278bbf64464ce03d5868d
DESKTOP-L9EVEPM\jihwankim <kimji22@plateer.com>
2023-04-28 14:40:32 +0900

    공통코드관리 bo 조회 -> api-bo 호출 작업

--- src/main/java/com/enbiz/api/bo/app/controller/system/CommonCodeMgmtController.java
+++ src/main/java/com/enbiz/api/bo/app/controller/system/CommonCodeMgmtController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.enbiz.api.bo.app.dto.request.realgrid.RealGridCUDRequest;
 import com.enbiz.api.bo.app.dto.request.system.GroupCodeRequest;
+import com.enbiz.api.bo.app.dto.request.system.StandardCodeMlRequest;
 import com.enbiz.api.bo.app.dto.request.system.StandardCodeRequest;
 import com.enbiz.api.bo.app.dto.response.system.GroupCodeResponse;
+import com.enbiz.api.bo.app.dto.response.system.StandardCodeMlResponse;
 import com.enbiz.api.bo.app.dto.response.system.StandardCodeResponse;
 import com.enbiz.api.bo.app.service.system.CommonCodeMgmtService;
 import com.enbiz.common.base.rest.Response;
@@ -27,51 +29,33 @@ public class CommonCodeMgmtController {
 	private final CommonCodeMgmtService commonCodeMgmtService;
 
 	/**
-	 * 그룹코드 목록 조회
-	 *
-	 * @param GroupCodeRequest
-	 * @return 그룹코드 목록
-	 * @throws Exception
-	 */
+     * 그룹코드 목록 조회
+     * @param groupCodeRequest
+     * @return 그룹코드 목록
+     * @throws Exception
+     */
 	@GetMapping("/getGroupCodeList")
 	Response<List<GroupCodeResponse>> getGroupCodeList(GroupCodeRequest groupCodeRequest) {
-		return new Response<List<GroupCodeResponse>>()
-				.setPayload(commonCodeMgmtService.getGroupCodeList(groupCodeRequest));
+		return new Response<List<GroupCodeResponse>>().setPayload(commonCodeMgmtService.getGroupCodeList(groupCodeRequest));
 	}
-
+	
 	/**
-	 * 그룹코드 목록 수 조회
-	 *
-	 * @param GroupCodeRequest
-	 * @return 그룹코드 목록 수
-	 * @throws Exception
-	 */
+     * 그룹코드 목록 수 조회
+     * @param  groupCodeRequest
+     * @return 그룹코드 목록 수
+     * @throws Exception
+     */
 	@GetMapping("/getGroupCodeListCount")
 	Response<Integer> getGroupCodeListCount(GroupCodeRequest groupCodeRequest) {
 		return new Response<Integer>().setPayload(commonCodeMgmtService.getGroupCodeListCount(groupCodeRequest));
 	}
 
-//    /**
-//     * 그룹코드 신규 등록
-//     * @param createList 신규 목록
-//     * @throws Exception
-//     */
-//    void registGroupCode(List<GroupCodeRequest> createList) throws Exception;
-//
-//    /**
-//     * 그룹코드 수정
-//     * @param updateList 수정 목록
-//     * @throws Exception
-//     */
-//    void modifyGroupCode(List<GroupCodeRequest> updateList) throws Exception;
-
 	/**
-	 * 그룹코드 저장 처리
-	 *
-	 * @param createList 신규 목록
-	 * @param updateList 수정 목록
-	 * @throws Exception
-	 */
+     * 그룹코드 저장 처리
+     * @param createList 신규 목록
+     * @param updateList 수정 목록
+     * @throws Exception
+     */
 	@PostMapping("/saveGroupCode")
 	Response<String> saveGroupCode(@RequestBody RealGridCUDRequest<GroupCodeRequest> realGridCUD) throws Exception {
 		List<GroupCodeRequest> createList = realGridCUD.getCreate(), updateList = realGridCUD.getUpdate();
@@ -79,51 +63,39 @@ public class CommonCodeMgmtController {
 		return new Response<String>().setPayload("success");
 	}
 
-	/**
-	 * 기준코드 목록 조회
-	 *
-	 * @param standardCodeRequest
-	 * @return 기준코드 목록
-	 * @throws Exception
-	 */
+    /**
+     * 기준코드 목록 조회
+     * @param  standardCodeRequest
+     * @return 기준코드 목록
+     * @throws Exception
+     */
 	@GetMapping("/getStandardCodeList")
 	Response<List<StandardCodeResponse>> getStandardCodeList(StandardCodeRequest standardCodeRequest) {
-		return new Response<List<StandardCodeResponse>>()
-				.setPayload(commonCodeMgmtService.getStandardCodeList(standardCodeRequest));
+		return new Response<List<StandardCodeResponse>>().setPayload(commonCodeMgmtService.getStandardCodeList(standardCodeRequest));
 	}
-
+	
 	/**
-	 * 기준코드 목록 수 조회
-	 *
-	 * @param StandardCodeRequest
-	 * @return 기준코드 목록
-	 * @throws Exception
-	 */
+     * 기준코드 목록 수 조회
+     * @param  standardCodeRequest
+     * @return 기준코드 목록
+     * @throws Exception
+     */
 	@GetMapping("/getStandardCodeListCount")
 	Response<Integer> getStandardCodeListCount(StandardCodeRequest standardCodeRequest) {
 		return new Response<Integer>().setPayload(commonCodeMgmtService.getStandardCodeListCount(standardCodeRequest));
 	}
-
-//    /**
-//     * 기준코드 신규 등록
-//     * @param createList 신규 목록
-//     * @throws Exception
-//     */
-//    void registStandardCode(List<StandardCodeRequest> createList) throws Exception;
-//
-//    /**
-//     * 기준코드 상세 수정 처리
-//     * @param updateList 수정 목록
-//     * @throws Exception
-//     */
-//    void modifyStandardCode(List<StandardCodeRequest> updateList) throws Exception;
-//
-//	/**
-//     * 기준코드 저장 처리
-//     * @param createList 신규 목록
-//     * @param updateList 수정 목록
-//     * @throws Exception
-//     */
-//    void saveStandardCode(List<StandardCodeRequest> createList, List<StandardCodeRequest> updateList) throws Exception;
-//
+	
+	/**
+     * 기준코드 저장 처리
+     * @param createList 신규 목록
+     * @param updateList 수정 목록
+     * @throws Exception
+     */
+	@PostMapping("/saveStandardCode")
+	Response<String> saveStandardCode(@RequestBody RealGridCUDRequest<StandardCodeRequest> realGridCUD) throws Exception {
+		List<StandardCodeRequest> createList = realGridCUD.getCreate(), updateList = realGridCUD.getUpdate();
+		commonCodeMgmtService.saveStandardCode(createList, updateList);
+		return new Response<String>().setPayload("success");
+    }
+	
 }
